5b2d0f5ff2e7d77ff9ed6014c23a18c80b3136fd
Florencia <sicref001@gmail.com>
2023-06-02 15:02:11 -0300

    Change imports

--- src/main/java/com/example/Library/model/Author.java
+++ src/main/java/com/example/Library/model/Author.java
@@ -4,15 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Setter @Getter
+
+@Setter
+@Getter
 @NoArgsConstructor
-@AllArgsConstructor @Builder
+@AllArgsConstructor
 @Entity
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
+    String name;
 }
--- src/main/java/com/example/Library/model/Book.java
+++ src/main/java/com/example/Library/model/Book.java
@@ -1,9 +1,23 @@
 package com.example.Library.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
+
 @Setter
 @Getter
 @NoArgsConstructor
